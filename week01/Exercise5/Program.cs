7cfac5911978f50bc6691503e701872222490e6f
oheneemmanuel <ohenekwakye24@gmail.com>
2025-03-09 12:12:46 -0800

    finished exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string user_name = PromptUserName();
+        int user_number = prompt_user_number();
+
+        int squared_number = square_number(user_number);
+
+        display_result(user_name, squared_number); 
+
     }
-}
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program");
+
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name : ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int prompt_user_number()
+    {
+        Console.Write("Enter your favourite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int square_number(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void display_result(string name, int square)
+    {
+        Console.WriteLine($"{name}, the sqaure of your number is {square}");
+    }
+
+}   
