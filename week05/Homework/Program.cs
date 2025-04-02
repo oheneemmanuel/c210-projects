3b0dfeeac35efcb17ba17d605c5303d0da5cb2f8
oheneemmanuel <ohenekwakye24@gmail.com>
2025-04-02 11:40:18 -0800

    homework done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment a1 = new Assignment("Ohene", "Decimals");
+        Console.WriteLine(a1.GetSummary());
+        
+        Console.WriteLine();
+    
+        MathAssignment math1 = new MathAssignment("Kwakye", "Fractions", "10.2", "13-18");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeWorkList());
+        Console.WriteLine();
+    
+        WritingAssignment writing1 = new WritingAssignment("Emmanuel", "The Great Laws", "The Principles of life");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInfo());
+        Console.WriteLine();
+
     }
 }
